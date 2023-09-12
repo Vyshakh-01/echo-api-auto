497bf5ca78560b04bf9d880350dbe05199b825f3
Vyshakh-01 <57756296+Vyshakh-01@users.noreply.github.com>
2023-09-12 16:01:47 +0530

    Working Cucumer run

---
+++ features/support/steps.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+const { When, Then } = require('@cucumber/cucumber')
+const { Greeter } = require('../../src')
+
+When('the greeter says hello', function () {
+  this.whatIHeard = new Greeter().sayHello()
+});
+
+Then('I should have heard {string}', function (expectedResponse) {
+    console.log('Hello world')
+    assert.equal(this.whatIHeard, expectedResponse)
+
+});
--- src/test/resources/features/Acceptance.feature
+++
@@ -1,9 +0,0 @@
-@Acceptance
-Feature: fghjk
-
-
-
-
-@test1
-Scenario: Hello World
-Given Say Hello World
